1ae74184be7f83b087231ef2a2b5605142e70857
TechnologyTim <techtim02@gmail.com>
2022-01-21 15:34:28 +0100

    added some fuckery

---
+++ index.js
@@ -0,0 +1,7 @@
+    const app = Vue.createApp({
+    components: {
+        
+    }
+})
+
+app.mount()
--- submit/submit.js
+++ submit/submit.js
@@ -1,28 +1,7 @@
-window.onload = function () {
-
-    var submitbutton = document.getElementById("submitbutton");
-    if (submitbutton.addEventListener) {
-        submitbutton.addEventListener("click", submitClick, false);
-    } else if (submitbutton.attachEvent) {
-        submitbutton.attachEvent("onclick", submitClick);
+Vue.createApp({
+    data() {
+        return {
+            name: ''
+        }
     }
-
-};
-
-function submitClick() {
-    
-    var username = document.getElementById("nameinput").value;
-    var category = (document.querySelector('input[name="category"]:checked').value);
-    var link = (document.getElementById("linkinput").value);
-    const pending = [];
-    
-    var combined = username + ", " + category  + ", " + link;
-    pending.push(combined);
-
-    //alert("FUCK");
-
-    /*export{
-        combined,
-        pending
-    };*/
-}
+}).mount('#v-model-basic')
